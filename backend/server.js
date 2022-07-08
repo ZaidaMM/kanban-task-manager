c07d49aa486932b7959f9bf2ffe031898973abba
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-07-08 17:08:28 +0100

    test route

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Welcome to Kanban Task Manager');
+  res.status(200).json({ message: 'Welcome to Kanban Task Manager' });
 });
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
