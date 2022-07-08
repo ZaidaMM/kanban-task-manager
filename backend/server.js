2c3369acecd21a740bbc2e4c2ab0c8515dc2eb55
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-07-08 16:50:19 +0100

    express server setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log('server running...');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+//Initialize express
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Kanban Task Manager');
+});
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
