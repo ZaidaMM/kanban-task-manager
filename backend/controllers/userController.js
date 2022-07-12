22947e534f0dcdb1715c57bd745d2c850ff2f8a9
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-07-12 17:12:00 +0100

    register user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,7 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/userModel');
 
 // desc:   Register New User
 // route:  /api/users
@@ -12,7 +15,30 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error('Please add all fileds');
   }
 
-  res.send('Register Route');
+  // Find if user already exists
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    res.status(400);
+    throw new Error('Email already in use');
+  }
+
+  // Hash the password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create User
+  const user = await User.create({ name, email, password: hashedPassword });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
 });
 
 // desc:   Login User
