51e64e2454ad0a5038e8791d465512b5d1b13083
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-07-12 14:04:25 +0100

    user routes setup

---
+++ backend/controllers/userController.js
@@ -0,0 +1,23 @@
+// desc:   Register New User
+// route:  /api/users
+// access: Public
+const registerUser = (req, res) => {
+  const { name, email, password } = req.body;
+
+  // Validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please add all fileds');
+  }
+
+  res.send('Register Route');
+};
+
+// desc:   Login User
+// route:  /api/users/login
+// access: Public
+const loginUser = (req, res) => {
+  res.send('Login Route');
+};
+
+module.exports = { registerUser, loginUser };
--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,14 @@ const PORT = process.env.PORT || 5000;
 //Initialize express
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to Kanban Task Manager' });
 });
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
