ccff9a46753898932f21b9aaea78f3d7594cd977
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-07-12 14:19:12 +0100

    erro handler setup

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { errorHandler } = require('./middleware/errorMiddleware');
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 
@@ -14,5 +15,7 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
