a018ef98af1443d394019a42d6548d2020e4e070
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-07-15 00:51:20 +0100

    removed react boilerplate, global context setup

---
+++ client/src/context/appContext.js
@@ -0,0 +1,22 @@
+import React, { useContext, useReducer } from 'react';
+import reducer from './reducer';
+
+const initialState = {
+  isLoading: false,
+};
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+  );
+};
+
+const useAppContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppProvider, initialState, useAppContext };
