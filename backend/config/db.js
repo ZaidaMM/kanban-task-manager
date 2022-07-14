3b03b4ca80dba9cd08e957ae04772ab9e382e022
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-07-14 22:33:45 +0100

    frontend created, dependencies

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB connected: ${conn.connection.host}`.red.underline);
+    console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
     console.log(`Error: ${error.message}`.red.underline.bold);
     process.exit(1);
