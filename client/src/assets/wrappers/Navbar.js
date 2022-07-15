06a39aec482c9d75f9096ce8a8a94131c49894a1
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-07-15 17:08:28 +0100

    navbar setup

---
+++ client/src/assets/wrappers/Navbar.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.nav`
+  width: 90vw;
+  height: 96px;
+  .nav-title {
+    line-height: 30.24px;
+  }
+`;
+
+export default Wrapper;
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,4 +1,19 @@
+import Wrapper from '../assets/wrappers/Navbar';
+import { ReactComponent as Ellipsis } from '../assets/images/icon-vertical-ellipsis.svg';
+
 const Navbar = () => {
-  return <div>This is the Navbar</div>;
+  return (
+    <Wrapper>
+      <div className='nav-center flex items-center justify-between py-7 w-full border-solid border-b-2 border-grey-200'>
+        <h1 className='nav-title lead '>Platform Launch</h1>
+        <div className='nav-buttons flex items-center '>
+          <button className=' block bg-purple-100 hover:opacity-25  opacity-30 text-white px-6 py-3.5  rounded-3xl'>
+            + Add New Task
+          </button>
+          <Ellipsis className='mx-6 cursor-pointer' />
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 export default Navbar;
