db46eb63ca2d142f7f701b443b7fce6be4edcb68
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-07-18 17:26:02 +0100

    sidebar content setup

--- client/src/assets/wrappers/Sidebar.js
+++ client/src/assets/wrappers/Sidebar.js
@@ -1,8 +1,46 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.aside`
-  width: 300px;
-  .logo {
+  border-right: var(--border);
+  /* display: none; */
+
+  @media (min-width: 992px) {
+    .sidebar-container {
+      display: block;
+      width: 300px;
+      min-height: 100vh;
+      height: 100%;
+      /* margin-left: -250px; */
+      transition: var(--transition);
+    }
+    .content {
+      position: sticky;
+      top: 32px;
+    }
+    .show-sidebar {
+      margin-left: 0;
+    }
+    .header {
+      padding-left: 34px;
+    }
+    .sidebar-title {
+      text-transform: uppercase;
+      color: var(--grey-300);
+      font-weight: 700;
+      letter-spacing: 2.4px;
+      padding-top: 70px;
+      font-size: 12px;
+      padding-left: 32px;
+      padding-bottom: 19px;
+    }
+    .board-list {
+      display: flex;
+      flex-direction: column;
+      padding-left: 32px;
+    }
+    .board-title {
+      padding-left: 32px;
+    }
   }
 `;
 
--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -4,13 +4,18 @@ import Logo from './Logo';
 const Sidebar = () => {
   return (
     <Wrapper>
-      <div className='sidebar border-solid border-r-2 border-grey-200 pt-9 pl-8'>
-        <Logo />
-
-        <h2>TITLE</h2>
-        <h3>Link1</h3>
-        <h3>Link2</h3>
-        <h3>Link3</h3>
+      <div className='sidebar-container '>
+        <div className='content show-sidebar'>
+          <div className='header'>
+            <Logo />
+          </div>
+          <p className='sidebar-title'>
+            All Boards <span>(3)</span>{' '}
+          </p>
+          <h3 className='board-title'>Board1</h3>
+          <h3 className='board-title'>Board2</h3>
+          <h3 className='board-title'>Board3</h3>
+        </div>
       </div>
     </Wrapper>
   );
