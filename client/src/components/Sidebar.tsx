ccd6d646c67afc40afb8466f2f4afbeb99956700
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-07-27 01:38:04 +0100

    boards count

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -5,9 +5,39 @@ import { ReactComponent as Icon } from '../assets/images/icon-board.svg';
 import ModeToggler from './ModeToggler';
 import SidebarToggler from './SidebarToggler';
 import { useAppContext } from '../context/appContext';
+import { useEffect, useState } from 'react';
 
 const Sidebar = () => {
   const { showSidebar, toggleSidebar } = useAppContext();
+  const [boards, setBoards] = useState([]);
+
+  const getBoards = () => {
+    fetch('http://localhost:5000/api/boards')
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((data) => {
+        setBoards(data);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log('error fetching data:', error);
+        // setError(error);
+      })
+      .finally(() => {
+        // setLoading(false);
+      });
+  };
+  console.log(boards);
+
+  useEffect(() => {
+    getBoards();
+    // eslint - disable - next - line;
+  }, []);
+  console.log(boards);
 
   return (
     <Wrapper>
@@ -21,7 +51,7 @@ const Sidebar = () => {
             <Logo />
           </header>
           <p className='sidebar-title'>
-            All Boards <span>(3)</span>{' '}
+            All Boards <span>({boards.length})</span>{' '}
           </p>
           <NavLinks />
           <div className='nav-link create-board'>
