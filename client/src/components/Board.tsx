9635ee01c57eb2ee7efa8358ac09c07115aa2ebf
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-08-30 12:01:15 +0200

    initial message update

--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -4,34 +4,43 @@ import Column from './Column';
 import InitialMessage from './InitialMessage';
 
 const Board = () => {
-  const { columns, selectedBoard } = useAppContext();
+  const { column, columns, selectedBoard } = useAppContext();
+
+  const columnsArr = [];
+  const col = selectedBoard?.columns;
+  console.log(col);
+  if (col?.length == 0) {
+    console.log(columns);
+    return <InitialMessage />;
+  }
+  if (!selectedBoard) {
+    return (
+      <Wrapper>
+        <></>
+      </Wrapper>
+    );
+  }
 
   return (
     <Wrapper>
       <div className='board-container'>
         <div className='columns-container'>
           <div className='columns'>
-            {selectedBoard?.columns ? (
-              <>
-                <div className='single-column'>
-                  {selectedBoard?.columns?.map((column) => (
-                    <Column
-                      key={column._id}
-                      name={column.name}
-                      _id={column._id}
-                      tasks={[]}
-                      customClass={`${column.name}`}
-                      quantity={columns?.length}
-                    />
-                  ))}
-                </div>
-                <div className='create-new-column'>
-                  <p>+ New Column</p>
-                </div>
-              </>
-            ) : (
-              <InitialMessage />
-            )}
+            <div className='single-column'>
+              {selectedBoard?.columns?.map((column) => (
+                <Column
+                  key={column._id}
+                  name={column.name}
+                  _id={column._id}
+                  tasks={[]}
+                  customClass={`${column.name}`}
+                  quantity={columns?.length}
+                />
+              ))}
+            </div>
+            <div className='create-new-column'>
+              <p>+ New Column</p>
+            </div>
           </div>
         </div>
       </div>
